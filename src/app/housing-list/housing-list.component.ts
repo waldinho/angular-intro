96418ff1c0da376e58aa784c51f1d9f2a757bd0b
waldinho <james.waller1984@gmail.com>
2023-06-22 15:28:23 +1000

    Add files via upload

---
+++ src/app/housing-list/housing-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { HousingLocation } from '../housing-location';
+
+@Component({
+  selector: 'app-housing-list',
+  templateUrl: './housing-list.component.html',
+  styleUrls: ['./housing-list.component.css'],
+})
+export class HousingListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  @Output() locationSelectedEvent = new EventEmitter<HousingLocation>();
+
+  @Input() locationList: HousingLocation[] = [];
+  results: HousingLocation[] = [];
+
+  @Input() selectedLocation: HousingLocation[] = [];
+
+  updateSelectedLocation(location: HousingLocation) {}
+
+  searchHousingLocations(searchText: string) {
+    if (!searchText) return;
+    this.results = this.locationList.filter((location: HousingLocation) =>
+      location.city.toLowerCase().includes(searchText.toLowerCase())
+    );
+  }
+
+  selectHousingLocation(location: HousingLocation) {
+    this.locationSelectedEvent.emit(location);
+  }
+}
